06c76c270e4767b9f32ac973889dc17642071805
Alok Prasad <143709815+alokprasad573@users.noreply.github.com>
2025-10-26 14:04:55 +0530

    api directory deleted and vercel.json modified.

--- api/index.js
+++
@@ -1,7 +0,0 @@
-import { createRequestHandler } from '@react-router/serve';
-import * as build from '../build/server/index.js'; // Adjust if needed
-
-export default createRequestHandler({
-  build,
-  mode: 'production',
-});
